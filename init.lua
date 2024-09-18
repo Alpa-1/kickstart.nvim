38058db7f19d55f9b593a9b16027703edc062e2a
Michael Siegle <michael.siegle@epg.com>
2024-09-18 14:04:35 +0200

    remove python for linting, add jinja support

--- init.lua
+++ init.lua
@@ -256,7 +256,6 @@ require('lazy').setup({
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
-
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
   --    require('gitsigns').setup({ ... })
@@ -569,11 +568,6 @@ require('lazy').setup({
             pyright = {
               disableOrganizeImports = true,
             },
-            python = {
-              analysis = {
-                ignore = { '*' },
-              },
-            },
           },
         },
         -- rust_analyzer = {},
@@ -601,6 +595,7 @@ require('lazy').setup({
             'typescript',
           },
         },
+        html = { filetypes = { 'html', 'htmldjango' } },
         volar = {},
         ruff_lsp = {},
         lua_ls = {
