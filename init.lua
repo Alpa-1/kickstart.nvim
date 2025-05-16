ed316b9983b9dc8560cca8f04783136bfa34029c
Michael Siegle <michael@fedora.fritz.box>
2025-05-16 11:41:15 +0200

    add flutter-tools

--- init.lua
+++ init.lua
@@ -963,6 +963,15 @@ require('lazy').setup({
       require('lsp_signature').setup(opts)
     end,
   },
+  {
+    'nvim-flutter/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      -- 'stevearc/dressing.nvim',   -- optional for vim.ui.select
+    },
+    config = true,
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
