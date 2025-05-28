c1cff3fa9aec3f2fcf53f7fd50254e596c3a15af
Michael Siegle <michael.sielge@mail.schwarz>
2025-05-28 13:49:31 +0200

    add auto-session plugin

--- init.lua
+++ init.lua
@@ -1009,7 +1009,19 @@ require('lazy').setup({
 
       -- see below for full list of options 👇
     },
+  },
+  {
+    'rmagatti/auto-session',
+    lazy = false,
 
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+      -- log_level = 'debug',
+    },
+  },
   require 'kickstart.plugins.lint',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
