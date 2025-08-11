d06af879ff37607be97466c4fcd5c49ae1ef6e2a
Michael Siegle <michael@fedora.fritz.box>
2025-08-12 00:53:36 +0200

    enable pyright and ts_ls

--- init.lua
+++ init.lua
@@ -599,6 +599,8 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
+        pyright = {},
+        ts_ls = {},
         gopls = {
           settings = {
             gopls = {
